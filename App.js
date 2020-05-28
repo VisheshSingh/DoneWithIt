353d7b0e8c76162f3828502e438c5a5c496b6872
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-27 22:15:32 -0500

    Alerts ⏰

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  Image,
-  View,
-  TouchableNativeFeedback,
-  Button,
-} from 'react-native';
+import { StyleSheet, SafeAreaView, Alert, Button } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log('Text pressed!');
   return (
     <SafeAreaView style={styles.container}>
-      <Button color='orange' title='click me' onPress={handlePress} />
+      <Button
+        color='orange'
+        title='click me'
+        onPress={() =>
+          Alert.alert('My title', 'My message', [
+            { text: 'Yes', onPress: () => console.log('Yes') },
+            { text: 'No', onPress: () => console.log('No') },
+          ])
+        }
+      />
     </SafeAreaView>
   );
 }
