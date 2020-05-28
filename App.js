f7105e294da1c4e469b8419ebaecca6dbb199539
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-27 21:35:53 -0500

    View 👀

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>DoneWithIt!</Text>
     </View>
   );
 }
@@ -12,8 +12,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
   },
 });
