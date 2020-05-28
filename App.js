3654575047617e13adb519443b201f8eaa076d8a
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-27 21:42:32 -0500

    Text 🀄

--- App.js
+++ App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log('Text pressed!');
   return (
     <View style={styles.container}>
-      <Text>DoneWithIt!</Text>
+      <Text numberOfLines={2} onPress={handlePress}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        consequuntur, at ipsa debitis praesentium culpa tempore hic vel? Aliquam
+        quasi ullam, ducimus delectus voluptate illum dolore aut eveniet quaerat
+        laborum beatae veritatis ipsam sunt excepturi sit odit dicta repellat
+        voluptatibus maxime sint officia hic, error perspiciatis ad! Temporibus,
+        dolore deleniti.
+      </Text>
     </View>
   );
 }
@@ -13,5 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'dodgerblue',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
