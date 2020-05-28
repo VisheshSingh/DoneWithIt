08999c7c3a168d75771da8a445da8082ac258650
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-27 22:09:13 -0500

    Buttons 🧈

--- App.js
+++ App.js
@@ -6,25 +6,14 @@ import {
   Image,
   View,
   TouchableNativeFeedback,
+  Button,
 } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log('Text pressed!');
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={2} onPress={handlePress} style={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-        consequuntur, at ipsa debitis praesentium culpa tempore hic vel? Aliquam
-        quasi ullam, ducimus delectus voluptate illum dolore aut eveniet quaerat
-        laborum beatae veritatis ipsam sunt excepturi sit odit dicta repellat
-        voluptatibus maxime sint officia hic, error perspiciatis ad! Temporibus,
-        dolore deleniti.
-      </Text>
-      <TouchableNativeFeedback onPress={() => console.log('Tapped on image!')}>
-        <View
-          style={{ width: 200, height: 70, backgroundColor: 'dodgerblue' }}
-        ></View>
-      </TouchableNativeFeedback>
+      <Button color='orange' title='click me' onPress={handlePress} />
     </SafeAreaView>
   );
 }
@@ -32,11 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
   },
 });
