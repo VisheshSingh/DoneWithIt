90e08a72a61e5a6d5534e716a4b52a6c7b11eb51
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-30 18:15:36 -0500

    Understanding flexbox 🍱

--- App.js
+++ App.js
@@ -1,51 +1,19 @@
 import React from 'react';
-import {
-  Dimensions,
-  StyleSheet,
-  SafeAreaView,
-  View,
-  Text,
-  Platform,
-  StatusBar,
-  YellowBox,
-} from 'react-native';
-
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from '@react-native-community/hooks';
-
+import { View, StyleSheet, YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Remote debugger']);
 
 export default function App() {
-  console.log(
-    'Dimensions: ',
-    useDimensions(),
-    'Orientation: ',
-    useDeviceOrientation()
-  );
-
-  const { landscape } = useDeviceOrientation();
-
-  const handlePress = () => console.log('Text pressed!');
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: 'dodgerblue',
-          width: '100%',
-          height: landscape ? '100%' : '30%',
-        }}
-      >
-        {/* <Text>DoneWithIt</Text> */}
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={{ backgroundColor: 'dodgerblue', flex: 2 }} />
+      <View style={{ backgroundColor: 'gold', flex: 1 }} />
+      <View style={{ backgroundColor: 'tomato', flex: 1 }} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
