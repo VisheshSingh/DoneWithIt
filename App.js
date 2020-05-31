2de02f725555b8854793ddf52bb80a8086d1f0b6
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-30 21:40:57 -0500

    expo/verctor icons 🧡💛💚

--- App.js
+++ App.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import { View, StyleSheet, YellowBox } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 import Welcome from './src/screens/Welcome';
 import ViewImageScreen from './src/screens/ViewImageScreen';
+
 YellowBox.ignoreWarnings(['Remote debugger']);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          backgroundColor: 'dodgerblue',
-          width: 100,
-          height: 100,
-          borderWidth: 10,
-          borderColor: 'royalblue',
-          elevation: 20,
-          // shadows don't work for android
-          shadowOffset: { width: 10, height: 10 },
-          shadowOpacity: 1,
-          shadowRadius: 10,
-          shadowColor: 'grey',
-        }}
-      ></View>
+      <MaterialCommunityIcons name='email' size={200} color='dodgerblue' />
     </View>
   );
 }
