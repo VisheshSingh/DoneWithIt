13c5b20616f064436d1ce1544b0f5f4d33ed05ca
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-31 16:05:43 -0500

    CellRenderer warning

--- src/screens/MessagesScreen.js
+++ src/screens/MessagesScreen.js
@@ -8,13 +8,13 @@ import ListItemDeleteAction from '../components/ListItemDeleteAction';
 
 const messages = [
   {
-    id: 1,
+    id: '1',
     title: 'T1',
     description: 'D1',
     image: require('../../assets/mosh.jpg'),
   },
   {
-    id: 2,
+    id: '2',
     title: 'T2',
     description: 'D2',
     image: require('../../assets/mosh.jpg'),
