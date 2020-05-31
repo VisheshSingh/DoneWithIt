8340930f06a3e4b0258548e97dc17beef3e20319
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-31 13:54:28 -0500

    applying styles

--- src/components/AppScreen.js
+++ src/components/AppScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 const AppScreen = ({ children }) => {
-  return <SafeAreaView>{children}</SafeAreaView>;
+  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
 };
 
 export default AppScreen;
