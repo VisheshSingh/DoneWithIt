da321c178c9cb50ff73cb1f9350e85b639da00e9
Vishesh Singh <VisheshSingh16@gmail.com>
2020-05-30 21:22:18 -0500

    Styling - Borders 🔳

--- App.js
+++ App.js
@@ -5,11 +5,26 @@ import ViewImageScreen from './src/screens/ViewImageScreen';
 YellowBox.ignoreWarnings(['Remote debugger']);
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: 'royalblue',
+          borderRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
