daf95c5f5b68cbb486b65ebf15f3a3e4b7590031
Vishesh Singh <VisheshSingh16@gmail.com>
2020-06-07 18:52:30 -0500

    pull to refresh WIP

--- src/screens/MessagesScreen.js
+++ src/screens/MessagesScreen.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import {
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  RefreshControl,
+} from 'react-native';
 import ListItem from '../components/ListItem';
 
 import AppScreen from '../components/AppScreen';
@@ -23,33 +29,55 @@ const initialMessages = [
     image: require('../../assets/mosh.jpg'),
   },
 ];
+
+function wait(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
+
 const MessagesScreen = (props) => {
   const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
 
   const handleDelete = (message) => {
     const newMessages = messages.filter((m) => m.id !== message.id);
     setMessages(newMessages);
   };
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, [refreshing]);
+
   return (
-    <AppScreen>
-      <FlatList
-        data={messages}
-        keyExtractor={(message) => message.id}
-        renderItem={({ item }) => (
-          <ListItem
-            title={item.title}
-            subTitle={item.description}
-            image={item.image}
-            onPress={() => console.log('List item: ', item)}
-            showChevrons
-            renderRightActions={() => (
-              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+    <SafeAreaView>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        <AppScreen>
+          <FlatList
+            data={messages}
+            keyExtractor={(message) => message.id}
+            renderItem={({ item }) => (
+              <ListItem
+                title={item.title}
+                subTitle={item.description}
+                image={item.image}
+                onPress={() => console.log('List item: ', item)}
+                showChevrons
+                renderRightActions={() => (
+                  <ListItemDeleteAction onPress={() => handleDelete(item)} />
+                )}
+              />
             )}
+            ItemSeparatorComponent={ListItemSeparator}
           />
-        )}
-        ItemSeparatorComponent={ListItemSeparator}
-      />
-    </AppScreen>
+        </AppScreen>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
